443bde259fd67ef5f881f9a4f9744abd419d452b
Entaro Adun <dcwmark@gmail.com>
2024-03-27 09:26:16 -0400

    02:46:42

--- common/utils.js
+++ common/utils.js
@@ -70,6 +70,7 @@ utils.invLerp = (a, b, v) => {
 utils.normalizePoints = (points, minMax) => {
   let min, max;
   const dimensions = points[0].length;
+
   if (minMax) {
     min = minMax.min;
     max = minMax.max;
@@ -82,14 +83,14 @@ utils.normalizePoints = (points, minMax) => {
         max[j] = Math.max(max[j], points[i][j]);
       }
     }
-    for (let i = 0; i < points.length; i++) {
-      for (let j = 0; j < dimensions; j++) {
-        points[i][j] =
-          utils.invLerp(min[j], max[j], points[i][j]);
-      }
+  }
+
+  for (let i = 0; i < points.length; i++) {
+    for (let j = 0; j < dimensions; j++) {
+      points[i][j] =
+        utils.invLerp(min[j], max[j], points[i][j]);
     }
   }
-  return { min, max };
 };
 
 if (typeof module !== 'undefined')
