4ef78222a930dc1db58afeeae0b6b577c06c793d
Entaro Adun <dcwmark@gmail.com>
2024-03-31 14:20:06 -0400

    knn.py

--- python/knn.py
+++ python/knn.py
@@ -9,29 +9,51 @@ classes = {
   'clock': 7
 }
 
+def readFeatureFile(filePath):
+  f = open(filePath, 'r')
+  lines = f.readlines()
+
+  X = []
+  y = []
+  # starting at 1 to skip the headers
+  for i in range(1, len(lines)):
+    # After the split, row is more like cols.
+    # In this case, the row would by an array of cols.
+    row = lines[i].split(',')
+    # print('line[' + str(i) + ']::' + lines[i])
+    # print('row::', row)
+    X.append(
+      # float conversion for the row of j
+      # in range of the length of the row - (minus) 1
+      # to exclude the last value, which is a label.
+      [float(row[j]) for j in range(len(row) - 1)]
+    )
+    y.append(
+      # the last value of a row, {code}row[-1]{code}, which is skipped in above
+      # is added here to Y
+      classes[row[-1].strip()]
+    )
+
+    return (X, y)
+
+# from sklearn.neighbors import KNeighborsClassifier
+# knn = KNeighborsClassifier(
+#   n_neighbors = 50,
+#   algorithm = "brute",
+#   weights = "uniform"
+# )
+
+# X, y = readFeatureFile("../data/dataset/training.csv")
 f = open("../data/dataset/training.csv", 'r')
 lines = f.readlines()
+print(lines)
+# print('X::', X[:10])
+# print(len(X))
+"""
+knn.fit(X, y)
 
-X = []
-Y = []
-# starting at 1 to skip the headers
-for i in range(1, len(lines)):
-  # After the split, row is more like cols.
-  # In this case, the row would by an array of cols.
-  row = lines[i].split(',')
-  # print('line[' + str(i) + ']::' + lines[i])
-  # print('row::', row)
-  X.append(
-    # float conversion for the row of j
-    # in range of the length of the row - (minus) 1
-    # to exclude the last value, which is a label.
-    [float(row[j]) for j in range(len(row) - 1)]
-  )
-  Y.append(
-    # the last value of a row, {code}row[-1]{code}, which is skipped in above
-    # is added here to Y
-    classes[row[-1].strip()]
-  )
+X, y = readFeatureFile("../data/dataset/testing.csv")
 
-print(X[:10])
-print(Y[:10])
+accuracy = knn.score(X, y)
+print("Accuracy::", accuracy)
+"""
